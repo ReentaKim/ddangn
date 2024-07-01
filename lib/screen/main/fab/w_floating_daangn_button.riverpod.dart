cc8d6b4991c23099d6f060f1b21e9058f64e908c
Reenta <reenta-kim@Reenta-M1-Pro.local>
2024-07-01 15:03:50 +0900

    Home Fab with Riverpod

---
+++ lib/screen/main/fab/w_floating_daangn_button.riverpod.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+final floatingButtonIsExpandedProvider = StateProvider<bool>((ref) => false);
+final floatingButtonIsSmallProvider = StateProvider<bool>((ref) => false);
--- lib/screen/main/tab/home/f_home.dart
+++ lib/screen/main/tab/home/f_home.dart
@@ -1,15 +1,41 @@
+import 'package:fast_app_base/screen/main/fab/w_floating_daangn_button.riverpod.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeFragment extends StatefulWidget {
+class HomeFragment extends ConsumerStatefulWidget {
   const HomeFragment({super.key});
 
   @override
-  State<HomeFragment> createState() => _HomeFragmentState();
+  ConsumerState<HomeFragment> createState() => _HomeFragmentState();
 }
 
-class _HomeFragmentState extends State<HomeFragment> {
+class _HomeFragmentState extends ConsumerState<HomeFragment> {
+  final scrollController = ScrollController();
+
+  @override
+  void initState() {
+    scrollController.addListener(() {
+      final isSmall = ref.read(floatingButtonIsSmallProvider);
+
+      if (scrollController.position.pixels > 100 && !isSmall) {
+        ref.read(floatingButtonIsSmallProvider.notifier).state = true;
+      } else if (scrollController.position.pixels < 100 && isSmall) {
+        ref.read(floatingButtonIsSmallProvider.notifier).state = false;
+      }
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView(
+      controller: scrollController,
+      children: [
+        Container(height: 500, color: Colors.red),
+        Container(height: 500, color: Colors.blue),
+        Container(height: 500, color: Colors.red),
+        Container(height: 500, color: Colors.blue),
+      ],
+    );
   }
 }
