79e6065908fea0322816e5f45243bd09e3e6035b
Reenta <reenta-kim@Reenta-M1-Pro.local>
2024-07-01 15:36:47 +0900

    당근마켓 - UI 그리기 03.홈-FAB(with Riverpod) 완료

--- lib/screen/main/fab/w_floating_daangn_button.riverpod.dart
+++ lib/screen/main/fab/w_floating_daangn_button.riverpod.dart
@@ -1,5 +1,39 @@
+import 'package:fast_app_base/screen/main/fab/w_floating_daangn_button.state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final floatingButtonStateProvider = StateNotifierProvider<FloatingButtonStateNotifier, FloatingButtonState>((ref) {
+  return FloatingButtonStateNotifier(
+    const FloatingButtonState(false, false),
+  );
+});
 
-final floatingButtonIsExpandedProvider = StateProvider<bool>((ref) => false);
-final floatingButtonIsSmallProvider = StateProvider<bool>((ref) => false);
+class FloatingButtonStateNotifier extends StateNotifier<FloatingButtonState> {
+  FloatingButtonStateNotifier(super.state);
+
+  bool needToMakeButtonBigger = false;
+
+  void onTapButton() {
+    final isExpanded = state.isExpanded;
+    final isSmall = state.isSmall;
+
+    state = state.copyWith(isExpanded: !state.isExpanded, isSmall: needToMakeButtonBigger ? false : true);
+
+    if (needToMakeButtonBigger) {
+      needToMakeButtonBigger = false;
+    }
+
+    if (!isSmall && !isExpanded) {
+      needToMakeButtonBigger = true;
+    }
+  }
+
+  void changeButtonSize(bool isSmall) {
+    if (state.isExpanded) {
+      return;
+    }
+
+    state = state.copyWith(
+      isSmall: isSmall,
+    );
+  }
+}
